e9159a587251dd77d7583fd554261f13e89dd437
LoveBye <lovebye@outlook.com>
2018-06-22 10:03:43 +0800

    Revert "添加BaseActivity和BaseTitleActivity"
    This reverts commit b7c07027473b1c86d6305beec5e7fddc958f6c3e.

--- app/src/main/java/com/example/myapplication/BaseActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseActivity.kt
@@ -1,13 +1,5 @@
 package com.example.myapplication
 
-import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 
-abstract class BaseActivity : FragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initListeners()
-    }
-
-    abstract fun initListeners()
-}
+open class BaseActivity : FragmentActivity()
