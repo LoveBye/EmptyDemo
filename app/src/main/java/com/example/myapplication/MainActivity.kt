678c526e73ad81ac3ae31099242eafa444901488
LoveBye <lovebye@outlook.com>
2018-06-22 10:01:31 +0800

    新引入BaseQuickAdapter

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.View
 import com.example.myapplication.Utils.LogUtils
 import com.example.myapplication.Utils.ToastUtils
 
 class MainActivity : BaseTitleActivity() {
+
+    override fun initListeners() {
+
+    }
+
+    override fun initViews() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val onClickListener = View.OnClickListener { ToastUtils.showToast(this, "点击了右边按钮") }
+        setRightButton(R.drawable.share_black, onClickListener)
+    }
+
     override fun setTitle(): String {
         return "我的第一个activity"
     }
 
     override fun setLayoutResource(): Int {
         return R.layout.activity_main
-    }
-
-    override fun initListeners() {
-        ToastUtils.showToast(this, "initListeners方法")
     }
 
     init {
